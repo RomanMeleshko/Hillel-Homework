8dc3269837157a56b9a06f30834d6b9e50a00941
RomanMeleshko <roman.meleshko1@gmail.com>
2018-11-10 17:38:07 +0200

    Д/З - Морзе

--- newjavascript.js
+++ newjavascript.js
@@ -1,28 +1,49 @@
-$( document ).ready(function() {
-
-     var even = $(".conteiner ul li:even").css("background-color", "orange");
-     var odd = $(".conteiner ul li:odd").css("background-color", "yellow");
-     $(function (){
-        $(".conteiner ul li").draggable({
-           revert: "invalid",
-           helper: "clone"
-
-      });
-
-      $(".conteiner:first-child").droppable({
-          accept: even,
-
-          drop: function(even, ul){
-              $(".left").append(ul.draggable);
+$( document ).ready(function(){
+   
+    var abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var morsecode =[
+    '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....',
+    '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.',
+    '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-',
+    '-.--', '--..'
+    ];    
+  
+   var anyText = "ROMAN MELESHKO"; 
+  
+ //Функция decoder() принимает в качекстве параметра текс для кодировки. 
+ function decoder(par){
+      var arr = par.split("");  //В переменной arr получаем массив символов.
+      var arrABC = abc.split(""); //В переменной arrABC получаем массив символов.
+      var str = "";
+      //Прохожу циклами для поиска и замены
+      for(var i = 0;i < arr.length;i++){
+          var x = arr[i];
+          if(arr[i] === " ") str += "";
+          for(var j = 0;j < arrABC.length;j++){
+              if(x === arrABC[j]) str += morsecode[j] + " ";
           }
-        });
-
-      $(".conteiner:last-child").droppable({
-          accept: odd,
-          drop: function(even, ul){
-              $(".right").append(ul.draggable);
+      }
+     return str; //Возврат в метод полученого результата.
+  }
+  console.log(decoder(anyText));
+  
+  var textMorsecode = decoder(anyText);
+  
+  //Функция encoder() получает в качестве параметра код Морзе.
+  function encoder(par){
+      var arr = par.split(" ");  //Полученный параметр сохраняем в массив arr по элементам. 
+      var arrABC = abc.split("");
+      var str = "";
+      //Прохожу циклами для поиска и замены (розкадировка).
+      for(var i = 0;i < arr.length;i++){
+          var x = arr[i];
+          for(var j = 0;j < morsecode.length;j++){
+              if(x === morsecode[j]) str += arrABC[j];
           }
-      });
-     });
-
+      }
+      
+      return str;
+  };
+  console.log(encoder(textMorsecode));
 });
+
